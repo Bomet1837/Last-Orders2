56aec3dbd413db32d796cf471c6fc5d09657b949
Jack <exploretheinternet32@gmail.com>
2025-10-02 01:16:35 +0100

    Basic Drink system
    the scene WAS edited

---
+++ Assets/Scripts/SadMutant.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SadMutant : Mutant
+{
+    void Start()
+    {
+        Drink(new UnhappyDrink());
+    }
+
+    public override void Drink<UnhappyDrink>(UnhappyDrink drink)
+    {
+        Debug.Log("Wow I hate life!");
+    }
+}
